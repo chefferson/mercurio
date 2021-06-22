cc172a7ace2b3025c4f05d692a68a8557cbdd95d
Jacob Santala <JacobISantala@gmail.com>
2021-06-22 10:51:04 -0700

    Made lots of progress with the initial setup. We got our basic server routes, installed dependencies, got our file structure dialed in.

---
+++ API/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const products = require('./products');
+const qa = require('./qa');
+const reviews = require('./reviews');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use('/products', products);
+app.use('/qa', qa);
+app.use('/reviews', reviews);
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`listening on port ${process.env.SERVER_PORT}`);
+});
