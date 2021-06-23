329335c0d75a579fd405d9b5d376a7b3cadc1dce
chefferson <49037676+chefferson@users.noreply.github.com>
2021-06-23 11:11:45 -0700

    - Added more config to example.env to specify credentials and config for each of the three DBs - Added a test record function to sequelize to test Review table

--- DATABASE/Reviews/ReviewsETL.js
+++ DATABASE/Reviews/ReviewsETL.js
@@ -2,30 +2,61 @@ const { Sequelize, DataTypes } = require('sequelize');
 
 require('dotenv').config();
 
+// Create the sequelize object with appropriate configuration
 const {
-  DB_TYPE,
-  DB_NAME,
-  DB_HOST,
-  DB_PORT,
-  DB_USER,
+  DB_REVIEWS_TYPE,
+  DB_REVIEWS_NAME,
+  DB_REVIEWS_HOST,
+  DB_REVIEWS_PORT,
+  DB_REVIEWS_USER,
 } = process.env;
-const sequelize = new Sequelize(`${DB_TYPE}://${DB_USER}@${DB_HOST}:${DB_PORT}/${DB_NAME}`);
+const sequelize = new Sequelize(`${DB_REVIEWS_TYPE}://${DB_REVIEWS_USER}@${DB_REVIEWS_HOST}:${DB_REVIEWS_PORT}/${DB_REVIEWS_NAME}`);
 
+// The model for the review table
 const Review = sequelize.define('Review', {
   id: {
-    type: DataTypes.UUID,
-    defaultValue: Sequelize.UUIDV4,
+    type: DataTypes.INTEGER,
     primaryKey: true,
+    unique: true,
   },
-  rating: DataTypes.INTEGER,
-  summary: DataTypes.STRING(512),
-  recommend: DataTypes.BOOLEAN,
-  response: DataTypes.TEXT,
-  body: DataTypes.TEXT,
-  reviewer_name: DataTypes.STRING,
-  helpfulness: DataTypes.INTEGER,
+  product_id: {
+    type: DataTypes.INTEGER,
+  },
+  rating: {
+    type: DataTypes.INTEGER,
+  },
+  summary: {
+    type: DataTypes.STRING(512),
+  },
+  body: {
+    type: DataTypes.TEXT,
+  },
+  recommend: {
+    type: DataTypes.BOOLEAN,
+  },
+  reported: {
+    type: DataTypes.BOOLEAN,
+  },
+  response: {
+    type: DataTypes.TEXT,
+  },
+  reviewer_name: {
+    type: DataTypes.STRING(100),
+  },
+  reviewer_email: {
+    type: DataTypes.STRING(100),
+  },
+  helpfulness: {
+    type: DataTypes.INTEGER,
+  },
+}, {
+  createdAt: 'date',
+  updatedAt: false,
 });
 
+// Synchronizes the Review table. Creates a table with the Review schema if it doesn't exist
+// already; if it does exist, I believe it will drop the table and create a new one, so you would
+// have to reimport data.
 const synchronize = async () => {
   try {
     await Review.sync({ force: true });
@@ -35,11 +66,32 @@ const synchronize = async () => {
   }
 };
 
+// Creates a test record for the review table
+const buildRecord = async () => {
+  const review = Review.build({
+    id: 39849,
+    product_id: 17071,
+    rating: 4,
+    summary: 'Not a bad product at all',
+    body: 'I really enjoyed using this product. It worked just as expected. It was a little too big, but that\'s my only complaint',
+    recommend: true,
+    reported: false,
+    response: 'Thanks for your review',
+    reviewer_name: 'reviewer_johnson',
+    reviewer_email: 'johnson_reviews@aol.com',
+    hepfulness: 12,
+  });
+  await review.save();
+};
+
+// Connects to the database, synchronizes the Reviews table, and creates a test record for review
+// table
 const connect = async () => {
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
     await synchronize();
+    await buildRecord();
     sequelize.close();
   } catch (error) {
     console.error('Unable to connect to the database:', error);
