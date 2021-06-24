b403677b30325f7e7332ff358316d14cce0d5990
chefferson <49037676+chefferson@users.noreply.github.com>
2021-06-24 15:43:02 -0700

    - Routes now extract appropriate data from requests

--- API/reviews.js
+++ API/reviews.js
@@ -3,28 +3,28 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  console.log('get /');
-  res.send('get /');
+  const params = req.body;
+  res.send();
 });
 
 router.get('/meta', (req, res) => {
-  console.log('get /meta');
-  res.send('get /meta');
+  const params = req.body;
+  res.send();
 });
 
 router.post('/', (req, res) => {
-  console.log('post /');
-  res.send('post /');
+  const params = req.body;
+  res.send();
 });
 
 router.put('/:review_id/helpful', (req, res) => {
-  console.log('put /:review_id/helpful');
-  res.send('put /:review_id/helpful');
+  const reviewID = req.params.review_id;
+  res.send();
 });
 
 router.put('/:review_id/report', (req, res) => {
-  console.log('put /:review_id/report');
-  res.send('put /:review_id/report');
+  const reviewID = req.params.review_id;
+  res.send();
 });
 
 module.exports = router;
