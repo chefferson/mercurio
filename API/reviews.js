344d07ce644da3e7a56143a3c526b822fda54067
chefferson <49037676+chefferson@users.noreply.github.com>
2021-06-24 15:50:28 -0700

    - Fixed server response statuses

--- API/reviews.js
+++ API/reviews.js
@@ -14,17 +14,17 @@ router.get('/meta', (req, res) => {
 
 router.post('/', (req, res) => {
   const params = req.body;
-  res.send();
+  res.sendStatus(201);
 });
 
 router.put('/:review_id/helpful', (req, res) => {
   const reviewID = req.params.review_id;
-  res.send();
+  res.sendStatus(204);
 });
 
 router.put('/:review_id/report', (req, res) => {
   const reviewID = req.params.review_id;
-  res.send();
+  res.sendStatus(204);
 });
 
 module.exports = router;
