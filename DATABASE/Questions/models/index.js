4b902f02bc7ce4e9da8b47e076c690402ecb426a
Will Weindel <will.weindel@gmail.com>
2021-06-26 10:05:40 -0700

    updates postgre index.js

--- DATABASE/Questions/models/index.js
+++ DATABASE/Questions/models/index.js
@@ -1,6 +1,15 @@
-const Sequelize = require('sequelize');
-const config = require('../config');
+const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(`postgres://${config.username}@${config.host}:${config.port}/${config.database}`);
+require('dotenv').config();
+
+const {
+  DB_QUESTIONS_TYPE,
+  DB_QUESTIONS_NAME,
+  DB_QUESTIONS_HOST,
+  DB_QUESTIONS_PORT,
+  DB_QUESTIONS_USER,
+} = process.env;
+
+const sequelize = new Sequelize(`${DB_QUESTIONS_TYPE}://${DB_QUESTIONS_USER}@${DB_QUESTIONS_HOST}:${DB_QUESTIONS_PORT}/${DB_QUESTIONS_NAME}`);
 
 module.exports.sequelize = sequelize;
