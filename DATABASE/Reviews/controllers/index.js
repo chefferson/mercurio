dc77c35ca3400f16486ab566ff2beb0bc9c6f00c
chefferson <49037676+chefferson@users.noreply.github.com>
2021-06-26 12:11:41 -0700

    - Removing unused controllers

--- DATABASE/Reviews/controllers/index.js
+++ DATABASE/Reviews/controllers/index.js
@@ -1,7 +1,4 @@
-exports.CharacteristicReview = require('./characteristicReview');
-exports.Characteristic = require('./characteristic');
 exports.Review = require('./review');
-exports.ReviewPhoto = require('./reviewPhoto');
 exports.reviewCreate = require('./reviewCreate');
 exports.markHelpful = require('./markHelpful');
 exports.reportReview = require('./reportReview');
