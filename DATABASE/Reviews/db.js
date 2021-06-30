7400b4a3b17c93467e16781acdc38a33a92e9fc5
chefferson <49037676+chefferson@users.noreply.github.com>
2021-06-30 09:58:14 -0700

    - Minor changes to config for connection to reviews DB

--- DATABASE/Reviews/db.js
+++ DATABASE/Reviews/db.js
@@ -8,9 +8,17 @@ const {
   DB_REVIEWS_HOST,
   DB_REVIEWS_PORT,
   DB_REVIEWS_USER,
+  DB_REVIEWS_PASSWORD,
 } = process.env;
 
-const sequelize = new Sequelize(`${DB_REVIEWS_TYPE}://${DB_REVIEWS_USER}@${DB_REVIEWS_HOST}:${DB_REVIEWS_PORT}/${DB_REVIEWS_NAME}`, {
+// const sequelize = new Sequelize(`${DB_REVIEWS_TYPE}://${DB_REVIEWS_USER}:${DB_REVIEWS_PASSWORD}@${DB_REVIEWS_HOST}:${DB_REVIEWS_PORT}/${DB_REVIEWS_NAME}`, {
+//   logging: false,
+// });
+
+const sequelize = new Sequelize(DB_REVIEWS_NAME, DB_REVIEWS_USER, DB_REVIEWS_PASSWORD, {
+  host: DB_REVIEWS_HOST,
+  port: DB_REVIEWS_PORT,
+  dialect: DB_REVIEWS_TYPE,
   logging: false,
 });
 
