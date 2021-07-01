4e34dfb84abc1e6845e7b1d59ad2791f4a06d8cf
chefferson <49037676+chefferson@users.noreply.github.com>
2021-07-01 09:37:14 -0700

    - Adding config changes for Sequelize to allow access to DBs on different hosts

--- DATABASE/Products/utils/sequelizeInit.js
+++ DATABASE/Products/utils/sequelizeInit.js
@@ -3,14 +3,17 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const {
-  DB_PRODUCTS_TYPE,
   DB_PRODUCTS_NAME,
   DB_PRODUCTS_HOST,
-  DB_PRODUCTS_PORT,
   DB_PRODUCTS_USER,
+  DB_PRODUCTS_PASSWORD,
 } = process.env;
 
-const sequelize = new Sequelize(`${DB_PRODUCTS_TYPE}://${DB_PRODUCTS_USER}@${DB_PRODUCTS_HOST}:${DB_PRODUCTS_PORT}/${DB_PRODUCTS_NAME}`);
+const sequelize = new Sequelize(`${DB_PRODUCTS_NAME}`, `${DB_PRODUCTS_USER}`, `${DB_PRODUCTS_PASSWORD}`, {
+  host: DB_PRODUCTS_HOST,
+  dialect: 'postgres',
+  logging: false,
+});
 
 module.exports = {
   sequelize,
